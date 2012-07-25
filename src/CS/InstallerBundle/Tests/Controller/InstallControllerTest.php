15f08b507376ceda0c1bbd20612d789f89e89b83
Pierre du Plessis <pierre@pcservice.co.za>
2012-07-25 08:47:01 +0200

    Removed duplicate test for CSInstallerBundle controller, and fixed class name for InstallController test

--- src/CS/InstallerBundle/Tests/Controller/InstallControllerTest.php
+++ src/CS/InstallerBundle/Tests/Controller/InstallControllerTest.php
@@ -4,7 +4,7 @@ namespace CS\InstallerBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class InstallControllerTest extends WebTestCase
 {
     public function testIndex()
     {
