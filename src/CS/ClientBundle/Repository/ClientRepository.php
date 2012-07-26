3e04e7e8e4da2180009e142960fa7b91c6660fa4
Pierre du Plessis <pierre@pcservice.co.za>
2012-07-26 15:19:57 +0200

    Added client repository and entity classes

---
+++ src/CS/ClientBundle/Repository/ClientRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CS\ClientBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * ClientRepository
+ *
+ * Custom Repository class for managing clients
+ */
+class ClientRepository extends EntityRepository {}
