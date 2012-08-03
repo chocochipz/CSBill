960ac7be1382f0ab0aad1fda5be12ea8515365f7
Pierre du Plessis <pierre@pcservice.co.za>
2012-08-03 23:35:57 +0200

    Optimized installer listener, to check if installer should run or not. TODO: when application is near complete, get better way of checking if application is installed

--- src/CS/InstallerBundle/Listener/RequestListener.php
+++ src/CS/InstallerBundle/Listener/RequestListener.php
@@ -25,32 +25,78 @@ class RequestListener
      * @DI\Inject("service_container")
      */
     public $container;
+    
+    /**
+     * Core paths for assets
+     * 
+     * @var array $core_paths
+     */
+    protected $core_paths = array('css', 'images', 'js');
+
+    /**
+     * Core routes
+     * 
+     * @var array $core_routes
+     */
+    protected $core_routes = array('_installer', '_profiler', '_wdt');
 
     /**
      * @DI\Observe("kernel.request", priority = 10)
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        $request = $event->getRequest();
+		$route = $event->getRequest()->get('_route');
+		
+		$map = array_map(function($route) use ($event){
+			
+				return strpos($event->getRequest()->getPathInfo(), $route);
+			}, $this->core_paths);
+		
+		if(!in_array($route, $this->core_routes) && !in_array(true, $map))
+		{
+			// first we check if we can connect to the database
+			try {
+				$this->container->get('database_connection')->connect();
+			} catch (\Exception $e) {
+				// TODO: if we can't connect to the database, check if the application is installed or not.
+				// If not, go to the installer. If application is already installed, then just display an error message
+				$response = new RedirectResponse($this->container->get('router')->generate('_installer'));
+			
+				return $event->setResponse($response);
+			}
+		
+			// TODO: check (settings table|composer.lock file) for current installed version. If version can't be found, run installer
+			// if version is older than available version, go to upgrade page (unless automatic update is activiated)
+			// (Should we automatically take user to upgrade page, or just notifu that a new version is available?)
+			
+			/**
+			 * Temporary Implemation
+			 */
 
-        $script = $request->getScriptName();
+			// check if the users table exists. If not, go to installer
+			$repository = $this->container->get('doctrine.orm.entity_manager')->getRepository('CSUserBundle:User');
+			
+			try {
+				$repository->find('u')->execute();
+			} catch(\PDOException $e)
+			{
+				$response = new RedirectResponse($this->container->get('router')->generate('_installer'));
 
-        $uri = $request->getRequestUri();
+				return $event->setResponse($response);
+			}
+			
+			// check if there are any users loaded. If not, run the installer
+			$users = $repository->findAll();
+			
+			// TODO: get different way of checking if the application is installed or not
+			if(count($users) === 0)
+			{
+				$response = new RedirectResponse($this->container->get('router')->generate('_installer'));
+			} else {
+				$response = new RedirectResponse($this->container->get('router')->generate('_dashboard'));
+			}
 
-        $route = str_replace($script, '', $uri);
-
-        if ($route === '') {
-            try {
-                $this->container->get('database_connection')->connect();
-            } catch (\Exception $e) {
-                $response = new RedirectResponse($this->container->get('router')->generate('_installer'));
-
-                return $event->setResponse($response);
-            }
-
-            $response = new RedirectResponse($this->container->get('router')->generate('_dashboard'));
-
-            return $event->setResponse($response);
-        }
+			return $event->setResponse($response);
+		}
     }
 }
