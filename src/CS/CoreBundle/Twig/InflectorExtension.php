10e67cb663cec2dce0751c2af07c38fbb6cb8297
Pierre du Plessis <pierre@pcservice.co.za>
2012-09-25 15:19:17 +0200

    Added Inflector Class and twig functions for inflector

---
+++ src/CS/CoreBundle/Twig/InflectorExtension.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the CSBill package.
+ *
+ * (c) Pierre du Plessis <info@customscripts.co.za>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CS\CoreBundle\Twig;
+
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @DI\Service("twig.inflector")
+ * @DI\Tag("twig.extension")
+ */
+class InflectorExtension extends \Twig_Extension
+{
+    /**
+     * @DI\Inject("inflector")
+     */
+    public $inflector;
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_Extension::getFilters()
+     */
+    public function getFilters()
+    {
+        return array('singular' => new \Twig_Filter_Method($this, 'singular', array('safe' => 'html')));
+    }
+
+    /**
+     * Converts a string to singular
+     * @param string $text
+     */
+    public function singular($text)
+    {
+        return $this->inflector->singularize($text);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see Twig_ExtensionInterface::getName()
+     */
+    public function getName()
+    {
+        return 'twig.extension';
+    }
+}
